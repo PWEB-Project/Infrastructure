f670eafa0ce7662d01288f7145681f85136d5497
David-Gabriel Ion <zdavid112z@gmail.com>
2022-05-20 12:24:48 +0300

    monitoring for rabbitmq and elasticsearch (#6)

--- service-prometheus-stack/main.tf
+++ service-prometheus-stack/main.tf
@@ -8,4 +8,53 @@ resource "helm_release" "prometheus" {
   values = [
     file("${path.module}/values.yaml")
   ]
+
+  depends_on = [
+    kubernetes_secret.prometheus-additional-configs,
+    kubernetes_config_map.grafana-dashboards-infra
+  ]
+}
+
+resource "helm_release" "prometheus-elasticsearch" {
+  name       = "prometheus-elasticsearch-exporter"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-elasticsearch-exporter"
+  version    = "4.12.0"
+  timeout    = 900
+
+  values = [
+    file("${path.module}/elasticsearch.yaml")
+  ]
+}
+
+resource "kubernetes_secret" "prometheus-additional-configs" {
+  metadata {
+    name = "prometheus-additional-configs"
+    labels = {
+      "sensitive" = "true"
+      "app"       = "prometheus"
+    }
+  }
+  data = {
+    "additional-configs.yaml" = file("${path.module}/additional-configs.yaml")
+  }
+}
+
+resource "kubernetes_config_map" "grafana-dashboards-infra" {
+  metadata {
+    name = "grafana-dashboard-infra"
+
+    labels = {
+      grafana_dashboard = 1
+    }
+
+    annotations = {
+      k8s-sidecar-target-directory = "/tmp/dashboards/Infra"
+    }
+  }
+
+  data = {
+    "rabbitmq.json"           = file("${path.module}/dashboards/rabbitmq.json")
+    "elasticsearch.json"      = file("${path.module}/dashboards/elasticsearch.json")
+  }
 }
